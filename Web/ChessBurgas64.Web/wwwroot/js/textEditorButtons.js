1270ee93af34c5bf2a35a0bb7328595743f8e05d
Daniel <64807656+DanielNinchev@users.noreply.github.com>
2022-04-04 01:02:51 +0300

    Integrated Trumbowyg WYSIWYG editor

---
+++ Web/ChessBurgas64.Web/wwwroot/js/textEditorButtons.js
@@ -0,0 +1,19 @@
+﻿$('#editor').trumbowyg({
+    btns: [
+        ['viewHTML'],
+        ['undo', 'redo'],
+        ['formatting'],
+        ['strong', 'em', 'del'],
+        ['foreColor', 'backColor'],
+        ['fontfamily'],
+        ['fontsize'],
+        ['superscript', 'subscript'],
+        ['link'],
+        ['insertImage'],
+        ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+        ['unorderedList', 'orderedList'],
+        ['horizontalRule'],
+        ['removeformat'],
+        ['fullscreen'],
+    ]
+});
